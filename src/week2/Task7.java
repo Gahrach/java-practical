59b86998448e9933e17b4650d0843b39ccda0418
Gahrach <hrachya1988@mail.ru>
2021-12-12 05:34:40 -0800

    "week 2"

--- src/week2/Task7.java
+++ src/week2/Task7.java
@@ -7,20 +7,18 @@ public class Task7 {
         Scanner scanner = new Scanner(System.in);
         int a = scanner.nextInt();
         int b = scanner.nextInt();
-        if (a > 10 || b != 10) {
-            System.out.println("both legal");
+        if (a <= 10 && b == 10 ) {
+            System.out.println ("both false");}
+        if (a <= 10 && b != 10) {
+            System.out.println("a false b true");
         }
-        if ( a <= 10 ){
-            System.out.println( "a illeg b leg");}
-            if (b == 10) {
-                System.out.println("a legal b illegal");
-            }
-
-            else {
-                System.out.println("both ill");
-
+        if (a > 10 && b ==10)
+        {
+            System.out.println("a true b false");}
+        if(a > 10 && b != 10) {
+            System.out.println("both true");
         }
+
 
     }
 }
-
--- src/week2/Task7_2.java
+++ src/week2/Task7_2.java
@@ -2,25 +2,23 @@ package week2;
 
 import java.util.Scanner;
 
-public class Task7_1 {
+public class Task7_2 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int a = scanner.nextInt();
         int b = scanner.nextInt();
-        if (a <= 0 && b == 0) {
+        if (a >= 0 && b >= 0) {
             System.out.println("both false");
         }
-        if (a <= 0 && b > 0) {
-            System.out.println("a false b true");
-        }
-        if (a > 0 && b <= 0) {
+        if (a < 0 && b >= 0) {
             System.out.println("a true b false");
-        }if(a > 0 && b > 0) {
+        }
+        if (a >= 0 && b < 0) {
+            System.out.println("a false b true");
+        } if (a < 0 && b < 0) {
             System.out.println("both true");
         }
 
     }
+
 }
-
-
-
